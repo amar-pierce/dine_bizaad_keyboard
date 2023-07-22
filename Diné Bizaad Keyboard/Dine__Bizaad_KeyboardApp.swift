bd3c401dfa663eb0d6f5e67f5678aeeaa8690429
Amari Pierce <52806983+amar-pierce@users.noreply.github.com>
2023-07-21 19:32:03 -0700

    keyboard layout implemented

---
+++ Diné Bizaad Keyboard/Dine__Bizaad_KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dine__Bizaad_KeyboardApp.swift
+//  Diné Bizaad Keyboard
+//
+//  Created by Amari Pierce on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Dine__Bizaad_KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
